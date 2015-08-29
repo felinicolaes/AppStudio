b1c3379964b54e80f3dc6cb7460c4342156dfe27
Feli Nicolaes <feli.nicolaes@student.uva.nl>
2015-08-29 17:31:54 +0200

    Comment added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
-	//Comments because I hope I can update versions
+	// Comments because I hope I can update versions
+	// More comments to see if they show up
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
