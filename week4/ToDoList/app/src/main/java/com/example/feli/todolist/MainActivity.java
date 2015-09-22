ee7adcfdbae8662c04f339f2f19ca8194fd65428
Feli Nicolaes <feli.nicolaes@student.uva.nl>
2015-09-22 10:09:11 +0200

    bugloze toDoList
    schijft nog niet weg etc

---
+++ week4/ToDoList/app/src/main/java/com/example/feli/todolist/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.feli.todolist;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ListView listView;
+    EditText addToDo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getBeginPreferences();
+        addToDo = (EditText) findViewById(R.id.editText);
+
+        items = new ArrayList<String>();
+
+        makeList();
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                deleteItem(position);
+                return false;
+            }
+        });
+
+        saveBeginPreferences();
+    }
+
+    public void getBeginPreferences() {
+
+    }
+
+    public void saveBeginPreferences() {
+
+    }
+
+    public void makeList() {
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1, items);
+        listView.setAdapter(adapter);
+    }
+
+
+    public void clickAdd(View view) {
+        if (!addToDo.getText().toString().equals("")) {
+            items.add(addToDo.getText().toString());
+            makeList();
+            hideKeyboard();
+            addToDo.setText("");
+        }
+    }
+
+    public void deleteItem(int i) {
+        items.remove(i);
+        makeList();
+        Toast.makeText(getApplicationContext(), "Item removed", Toast.LENGTH_LONG).show();
+    }
+
+
+    //Code copied from http://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+    public void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
